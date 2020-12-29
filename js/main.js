fbbbf53385f56bccb80dfa3b7aaf3f88658f2e1e
Rupam <hrupam55@gmail.com>
2020-12-30 01:11:28 +0530

    made more responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(() => {
   $("#about h1")
     .addClass("animate__animated animate__zoomInLeft")
-    .css("--animate-duration", "950ms");
+    .css("--animate-duration", "1500ms");
 
-  $("#about p")
-    .addClass("animate__animated animate__zoomInRight")
-    .css("--animate-duration", "1000ms");
+  $("#about p").addClass(
+    "animate__animated animate__fadeInRight animate__slow"
+  );
 });
